6c9647375abe0726d61fcf3edb40a71df0c065b0
wenjiangit <wenjianes@163.com>
2017-06-01 23:02:35 +0800

    创建简单的api接口

--- web/src/main/java/net/qiujuer/web/italker/push/Application.java
+++ web/src/main/java/net/qiujuer/web/italker/push/Application.java
@@ -1,7 +1,25 @@
 package net.qiujuer.web.italker.push;
 
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import net.qiujuer.web.italker.push.service.AccountService;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.util.logging.Logger;
+
 /**
  * Created by wenjian on 2017/6/1.
  */
-public class Application {
+public class Application extends ResourceConfig {
+    public Application() {
+//        packages("net.qiujuer.web.italker.push.service");
+        packages(AccountService.class.getPackage().getName());
+
+        //注册日志输出类
+        register(Logger.class);
+
+        //注册json解析类
+        register(JacksonJsonProvider.class);
+
+
+    }
 }
---
+++ web/src/main/java/net/qiujuer/web/italker/push/service/AccountService.java
@@ -0,0 +1,32 @@
+package net.qiujuer.web.italker.push.service;
+
+import net.qiujuer.web.italker.push.bean.User;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Created by wenjian on 2017/6/1.
+ */
+@Path("/account")
+public class AccountService {
+
+    //restful请求,通过同一访问路径,可以识别post和get
+
+    @GET
+    @Path("login")
+    public String get() {
+        return "you get login";
+    }
+
+    @POST
+    @Path("login")
+    @Produces(MediaType.APPLICATION_JSON)//指定发送的数据类型
+    @Consumes(MediaType.APPLICATION_JSON)//指定接收的数据类型
+    public User post() {
+        User user = new User();
+        user.setName("wenjian");
+        user.setSex(1);
+        return user;
+    }
+}
